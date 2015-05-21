a660781c4cbd30fc2e1c63baab4039c4ae5924df
Meredith Rawls <meredith.rawls@gmail.com>
2015-05-21 15:28:52 -0700

    now uses the read_specfiles function

--- splot.py
+++ splot.py
@@ -4,15 +4,33 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 #from astropy.io.fits import getdata
 #from astropy.io.fits import getheader
+from BF_functions import read_specfiles
 '''
 Simple python script for plotting spectra from FITS files
 Meredith Rawls, originally written May 2014
+
+To run: python splot.py infiles.txt
 '''
 
 # CHANGE AXIS RANGE AS DESIRED
 #plt.axis([7590, 7720, -0.5, 25])
 #plt.axis([6100, 6300, -0.5, 25])
-plt.axis([5000, 8000, -0.5, 10])
+#plt.axis([5000, 8000, -0.5, 10])
+
+import sys
+infiles = sys.argv[1]
+bjdfile = '../../FDBinary/9246715/bjds_baryvels_apogee.txt'
+	# note: bcvin is never really used here, but you have to give it some file
+	# with the correct length because of how read_specfiles is set up
+#linelist = '../../MOOG/ares_v1.0/yong_fe2_jeanversion.txt'
+#refspec = '../../../Dropbox/KIC9246715/phoenix_air_jean.txt'
+refspec = '../../RG_spectra/APOGEE/model_rg_apogee.txt'
+isAPOGEE = False
+#if 'apogee' in infiles:
+#	isAPOGEE = True
+#	print('Setting isAPOGEE to True')
+#else:
+#	isAPOGEE = False
 
 # Option: draw vertical lines for comparison of feature location by eye
 # (the vertical lines will be every 2 Angstroms between xstart and xend)
@@ -25,46 +43,50 @@ plt.xlabel("Wavelength")
 plt.ylabel("Arbitrary Flux")
 
 # Read in a text file containing a list of fits files
-infiles = "infiles.txt"
+#infiles = "infiles.txt"
 
 i = 0.0
 yoffset = 0.5
-for line in open(infiles):
-	infile = line.rstrip()
-	# Read in the FITS file with all the data in the primary HDU
-	hdu = fits.open(infile)
-	spec = hdu[0].data + 2*i
-	head = hdu[0].header
-	try:
-		datetime = head['date-obs']
-	except:
-		datetime = head['date']
-	exptime = head['exptime']
 
-	# Define the wavelength scale (we need an x-axis for the data)
-	dwave = head['cdelt1']
-	wavestart = head['crval1']
-	wavestop = wavestart + dwave*len(spec)
-	wave = np.arange(wavestart, wavestop, dwave)
-	# This is here because the wave array was sometimes 1 longer than it should be?!
-	if len(wave) != len(spec):
-		minlength = min(len(wave), len(spec))
-		wave = wave[0:minlength]
-		spec = spec[0:minlength]
+### This only worked for FITS infiles in a standard format
+### I replaced it with a more generic call to read_specfiles
+# for line in open(infiles):
+# 	infile = line.rstrip()
+# 	# Read in the FITS file with all the data in the primary HDU
+# 	hdu = fits.open(infile)
+# 	spec = hdu[0].data + 2*i
+# 	head = hdu[0].header
+# 	try:
+# 		datetime = head['date-obs']
+# 	except:
+# 		datetime = head['date']
+# 	exptime = head['exptime']
+# 
+# 	# Define the wavelength scale (we need an x-axis for the data)
+# 	dwave = head['cdelt1']
+# 	wavestart = head['crval1']
+# 	wavestop = wavestart + dwave*len(spec)
+# 	wave = np.arange(wavestart, wavestop, dwave)
+# 	# This is here because the wave array was sometimes 1 longer than it should be?!
+# 	if len(wave) != len(spec):
+# 		minlength = min(len(wave), len(spec))
+# 		wave = wave[0:minlength]
+# 		spec = spec[0:minlength]	
 	
-	#print infile
-	#print len(wave)
-	#print len(pixel)
-	#print len(spec)	
-	
+nspec, filenamelist, datetimelist, wavelist, speclist, source = read_specfiles(infiles, bjdfile, isAPOGEE)
+for i, (wave, spec, datetime) in enumerate(zip(wavelist, speclist, datetimelist)):
 	# Add this data to the plot
-	plt.plot(wave, spec, color='b', marker='.', mfc='k', label=datetime[0:10])
+	plt.plot(wave, spec + 2*i, color='b', marker='.', mfc='k')#
 	i = i + yoffset
-	
-	#print (infile, datetime)
+
+# Plot text file reference spectrum here
+waveref, fluxref = np.loadtxt(refspec, usecols=(0,1), unpack=True)
+fluxref = fluxref + 2*i #apply yoffset
+plt.plot(waveref, fluxref, color='r', marker='.', mfc='k')
 
 # Plot random vertical lines here
-#plt.axvline(x=7676.55, color='0.75')
+#xvals = np.loadtxt(linelist, comments='#', usecols=(0,), unpack=True)
+#for xval in xvals:
+#	plt.axvline(x=xval, color='0.75')
 
-#plt.legend()
 plt.show()
