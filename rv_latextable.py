47d72588e2a37c3b818127d5d5a51375aa94763d
Meredith Rawls <meredith.rawls@gmail.com>
2016-03-02 21:35:41 -0800

    added options for SB1 vs SB2

--- rv_latextable.py
+++ rv_latextable.py
@@ -2,26 +2,45 @@ from __future__ import print_function
 import numpy as np
 from astropy.time import Time
 '''
-Read in Kepler BJD, RV, RVerror for boths tars
+Read in Kepler BJD, RV, RVerror for both stars
 Also get orbital period and zeropoint to calculate phases
+(this information is read from the 'trialresultfile')
 
 Return formatted LaTeX table entries as follows:
 UTC Date, Midpoint in BJD-2450000, Phase, RV1(err), RV2(err)
 '''
 dir = '../../RG_ELCmodeling/'
 
-#KIC = '3955867'
+#KIC = '3955867'; SB1 = False
 #trialresultfile = 'trials5_6_results.txt'
 
-#KIC = '10001167'
+#KIC = '10001167'; SB1 = False
 #trialresultfile = 'trial1_results.txt'
 
-KIC = '7037405'
-trialresultfile = 'trial7_finalresults.txt'
+#KIC = '7037405'; SB1 = False
+#trialresultfile = 'trial7_finalresults.txt'
 
-MSinfile = dir + KIC + '/rvs_' + KIC +'_MS.txt'
-RGinfile = dir + KIC + '/rvs_' + KIC +'_RG.txt'
+#KIC = '8702921'; SB1 = True
+#trialresultfile = 'trial3_175kmodels_results.txt'
+
+#KIC = '5786154'; SB1 = False
+#trialresultfile = 'trial2_921kmodels_results.txt'
+
+#KIC = '9970396'; SB1 = False
+#trialresultfile = 'trial4_500kmodels_results.txt' # NOT FINAL ANSWER YET
+
+KIC = '9291629'; SB1 = False
+trialresultfile = 'trials2_3_results.txt' # NOT FINAL ANSWER YET
+
+if SB1 == False:
+    MSinfile = dir + KIC + '/rvs_KIC_9291629_MS.txt'
+RGinfile = dir + KIC + '/rvs_KIC_9291629_RG.txt'
 ELCresults = dir + KIC + '/' + trialresultfile
+
+#MSinfile = dir + KIC + '/rvs_' + KIC +'_MS.txt'
+#RGinfile = dir + KIC + '/rvs_' + KIC +'_RG.txt'
+
+#print(MSinfile, RGinfile)
 
 def phasecalc(times, period=100, BJD0=2454833):
 	'''
@@ -70,22 +89,26 @@ for row in ELCoutdata:
 
 print('KIC ' + KIC +':', period, t0) # this zeropoint is in units of Kepler BJD!
 
-MStimes, MSrvs, MSrverrs = np.loadtxt(MSinfile, unpack=True)
+if SB1 == False:
+    MStimes, MSrvs, MSrverrs = np.loadtxt(MSinfile, unpack=True)
 RGtimes, RGrvs, RGrverrs = np.loadtxt(RGinfile, unpack=True)
 
 # calculate orbital phases in Kepler BJD units
-MSphases = phasecalc(MStimes, period, t0)
+if SB1 == False:
+    MSphases = phasecalc(MStimes, period, t0)
 RGphases = phasecalc(RGtimes, period, t0)
 
 # put into real BJD after calculating orbital phases
-MStimes = Time(MStimes + 2454833, format='jd', scale='utc')
+if SB1 == False:
+    MStimes = Time(MStimes + 2454833, format='jd', scale='utc')
 RGtimes = Time(RGtimes + 2454833, format='jd', scale='utc')
 
-MStimestrings = []
+if SB1 == False:
+    MStimestrings = []
+    for time in MStimes:
+        month = numToTxtMonth(str(time.iso[5:7]))
+        MStimestrings.append(str(time.iso[0:4]) + ' ' + month + ' ' + str(time.iso[8:10]))
 RGtimestrings = []
-for time in MStimes:
-    month = numToTxtMonth(str(time.iso[5:7]))
-    MStimestrings.append(str(time.iso[0:4]) + ' ' + month + ' ' + str(time.iso[8:10]))
 for time in RGtimes:
     month = numToTxtMonth(str(time.iso[5:7]))
     RGtimestrings.append(str(time.iso[0:4]) + ' ' + month + ' ' + str(time.iso[8:10]))
@@ -94,7 +117,7 @@ for time in RGtimes:
 for time, BJD, phase, rvRG, rverrRG in zip(RGtimestrings, RGtimes, RGphases, RGrvs, RGrverrs):
     errstringRG = format(rverrRG, '.2f')[2:]
     if errstringRG[0] == '0': errstringRG = errstringRG[1:]
-    if BJD in MStimes:
+    if SB1 == False and BJD in MStimes:
         idx = np.where(MStimes == BJD)
         rvMS = MSrvs[idx[0][0]]
         rverrMS = MSrverrs[idx[0][0]]
